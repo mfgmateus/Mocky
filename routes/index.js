85c5cdc40c82b2896052d4a526e5e5da9d4ff6f8
Mateus Ferreira <mfgmateus1996@gmail.com>
2017-01-10 22:58:03 -0200

    Adding initial files

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+var lineReader = require('readline').createInterface({
+    input: require('fs').createReadStream('conf.txt')
+});
+
+lineReader.on('line', function (line) {
+  var method = line.split('=')[0];
+  var path = line.split('=')[1];
+  var response = line.split('=')[2];
+  if(method == "POST")
+    router.post(path, function(req, res, next){
+      let responseJson = require(response);
+      res.send(responseJson);
+    });
+  else{
+    router.get(path, function(req, res, next){
+      let responseJson = require(response);
+      res.send(responseJson);
+    });
+  }
+});
+
+/* GET home page. */
+router.get('/*', function(req, res, next) {
+              console.log(req);
+              next();
+});
+
+module.exports = router;
